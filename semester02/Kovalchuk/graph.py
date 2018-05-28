ba8f9be9ba5cd54c1bafd4ea5e232142adfa3581
Oleksandr Kovalchuk <anxolerd@outlook.com>
2018-05-28 21:14:37 +0300

    WIP: labs and algorithms

---
+++ semester02/Kovalchuk/graph.py
@@ -0,0 +1,228 @@
+from itertools import chain
+
+
+class _Gen(object):
+    def __init__(self):
+        self.__value = 0
+
+    def __call__(self):
+        self.__value += 1
+        return self.__value
+
+
+class Node(object):
+    
+    def __init__(self, id_, weight):
+        self._id = id_
+        self._weight = weight
+        self._incoming = {}
+        self._outgoing = {}
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def weight(self):
+        return self._weight
+
+    def connect(self, target, weight):
+        if target.id in self._outgoing:
+            raise ValueError(f'{self} is already connected to {target}')
+        edge = Edge(self, target, weight)
+        self._outgoing[target.id] = edge
+        target._incoming[self.id] = edge
+
+    def disconnect(self, target):
+        if target.id in self._incoming:
+            del self._incoming[target.id]
+            del target._outgoing[self.id]
+        if target.id in self._outgoing:
+            del self._outgoing[target.id]
+            del target._incoming[self.id]
+
+    @property
+    def is_start_node(self):
+        return len(self._incoming) == 0
+
+    @property
+    def is_end_node(self):
+        return len(self._outgoing) == 0
+
+    @property
+    def conns(self):
+        return self.conns_in + self.conns_out
+
+    @property
+    def conns_in(self):
+        return len(self._incoming)
+
+    @property
+    def conns_out(self):
+        return len(self._outgoing)
+
+    @property
+    def connections(self):
+        return chain(self._incoming.values(), self._outgoing.values())
+
+    @property
+    def connections_in(self):
+        return iter(self._incoming.values())
+
+    @property
+    def connections_out(self):
+        return iter(self._outgoing.values())
+
+    def __str__(self):
+        return f'Node_{self._id}({self._weight})'
+
+    def __repr__(self):
+        return str(self)
+
+
+class Edge(object):
+    def __init__(self, source, target, weight):
+        self._source = source
+        self._target = target
+        self._weight = weight
+
+    @property
+    def id(self):
+        return (self._source.id, self._target.id)
+
+    @property
+    def weight(self):
+        return self._weight
+
+    @property
+    def source(self):
+        return self._source
+
+    @property
+    def target(self):
+        return self._target
+
+
+
+class Graph(object):
+    def __init__(self):
+        self._gen = _Gen()
+        self._nodes = {}
+        self.__frozen = False
+
+    def add_node(self, weight):
+        if self.__frozen:
+            raise ValueError('Cannot add node to the frozen graph')
+        node = Node(self._gen(), weight=weight)
+        self._nodes[node.id] = node
+
+    def del_node(self, node):
+        if self.__frozen:
+            raise ValueError('Cannot del node from the frozen graph')
+        if isinstance(node, int):
+            node = self._nodes[node]
+        for other in node.connections:
+            node.disconnect(other)
+        del self._nodes[node.id]
+
+    def connect(self, source, target, weight):
+        if self.__frozen:
+            raise ValueError('Cannot make conns in the frozen graph')
+        if isinstance(source, int):
+            source = self._nodes[source]
+        if isinstance(target, int):
+            target = self._nodes[target]
+        source.connect(target, weight)
+
+    def disconnect(self, source, target):
+        if self.__frozen:
+            raise ValueError('Cannot del conns in the frozen graph')
+        if isinstance(source, int):
+            source = self._nodes[source]
+        if isinstance(target, int):
+            target = self._nodes[target]
+        source.disconnect(target)
+
+    def freeze(self):
+        self.__frozen = True
+        self._reindex()
+        self._start_nodes = tuple(n for n in self._nodes.values()
+                                  if n.is_start_node)
+        self._end_nodes = tuple(n for n in self._nodes.values()
+                                if n.is_end_node)
+
+    def _reindex(self):
+        gen = _Gen()
+        mapping = {node.id: gen() for node in self._nodes.values()}
+        for node in self._nodes.values():
+            node._id = mapping[node.id]
+            node._outgoing = {
+                mapping[id_]: node._outgoing[id_]
+                for id_ in node._outgoing.keys()
+            }
+            node._incoming = {
+                mapping[id_]: node._incoming[id_]
+                for id_ in node._incoming.keys()
+            }
+        self._nodes = {node.id: node for node in self._nodes.values()}
+
+    @property
+    def start_nodes(self):
+        if self.__frozen:
+            return self._start_nodes
+        else:
+            return tuple(n for n in self._nodes if n.is_start_node)
+
+    @property
+    def end_nodes(self):
+        if self.__frozen:
+            return self._end_nodes
+        else:
+            return tuple(n for n in self._nodes if n.is_end_node)
+
+    @property
+    def frozen(self):
+        return self.__frozen
+
+    def __getitem__(self, item_id):
+        return self._nodes[item_id]
+
+    def __str__(self):
+        node_descs = []
+        edge_descs = []
+
+        for node in self._nodes.values():
+            node_descs.append(
+                f'\tNode_{node.id} [label="{node.id} ({node.weight})"];'
+            )
+            for edge in node.connections_out:
+                edge_descs.append(
+                    f'\tNode_{edge.source.id} -> Node_{edge.target.id} '
+                    f'[label="{edge.weight}"];'
+                )
+        return '\n'.join([
+            'digraph TheGraph {',
+            '\n'.join(node_descs),
+            '\n'.join(edge_descs),
+            '}'
+        ])
+
+
+if __name__ == '__main__':
+    g = Graph()
+    g.add_node(2)
+    g.add_node(3)
+    g.add_node(1)
+    g.add_node(3)
+    g.add_node(1)
+    g.add_node(1)
+    g.add_node(1)
+    g.add_node(4)
+    g.connect(1, 4, 1)
+    g.connect(2, 4, 1)
+    g.connect(2, 6, 1)
+    g.connect(3, 5, 1)
+    g.connect(4, 6, 1)
+    g.connect(6, 7, 1)
+    g.connect(6, 8, 1)
+    print(g)
---
+++ semester02/Kovalchuk/task_graph.py
@@ -0,0 +1,172 @@
+from collections import defaultdict
+from queue import Queue
+
+
+FAKE_NODE_ID = None
+PATH_END = 'critical_path_end'
+PATH_END_NODE = 'critical_path_end_node'
+PATH_START = 'critical_path_start'
+PATH_START_NODE = 'critical_path_start_node'
+CONN_IN = 'conn_in'
+CONN_OUT = 'conn_out'
+CONN = 'conn'
+EARLY_START = 'early_start'
+LATE_START = 'late_start'
+
+
+class TaskGraph(object):
+    def __init__(self, graph):
+        assert g.frozen
+        self.g = graph
+        self._nodes = defaultdict(dict)
+        self._critical_graph = 0
+        self._critical_graph_nodes = 0
+        self._make_metrics()
+
+    def _make_metrics(self):
+        start_nodes = self.g.start_nodes
+        end_nodes = self.g.end_nodes
+
+        todo = Queue()
+
+        for node in end_nodes:
+            todo.put((FAKE_NODE_ID, node))
+        while not todo.empty():
+            (next_id, n) = todo.get()
+            path_end = self._nodes[next_id].get(PATH_END, 0) + n.weight
+            path_end_node = self._nodes[next_id].get(PATH_END_NODE, 0) + 1
+
+            self._nodes[n.id][PATH_END] = max(
+                self._nodes[n.id].get(PATH_END, 0),
+                path_end,
+            )
+            self._nodes[n.id][PATH_END_NODE] = max(
+                self._nodes[n.id].get(PATH_END_NODE, 0),
+                path_end_node,
+            )
+
+            for prev in n.connections_in:
+               todo.put((n.id, prev.source))
+
+        for node in start_nodes:
+            todo.put((FAKE_NODE_ID, node))
+        while not todo.empty():
+            (pid, n) = todo.get()
+            if n.is_start_node:
+                path_start = 0
+            else:
+                prev = self.g[pid]
+                path_start = self._nodes[pid].get(PATH_START, 0) + prev.weight
+            path_start_node = self._nodes[pid].get(PATH_START_NODE, 0) + 1
+
+            self._nodes[n.id][PATH_START] = max(
+                self._nodes[n.id].get(PATH_START, 0),
+                path_start,
+            )
+            self._nodes[n.id][PATH_START_NODE] = max(
+                self._nodes[n.id].get(PATH_START_NODE, 0),
+                path_start_node,
+            )
+
+            for next_ in n.connections_out:
+               todo.put((n.id, next_.target))
+        del self._nodes[FAKE_NODE_ID]
+
+        self._critical_graph = max(
+            map(lambda n: n[PATH_END], self._nodes.values())
+        )
+        self._critical_graph_nodes = max(
+            map(lambda n: n[PATH_END_NODE], self._nodes.values())
+        )
+
+        for nid in self._nodes:
+            node = self.g[nid]
+            self._nodes[nid][CONN_IN] = node.conns_in
+            self._nodes[nid][CONN_OUT] = node.conns_out
+            self._nodes[nid][CONN] = node.conns
+            self._nodes[nid][EARLY_START] = self._nodes[nid][PATH_START] + 1
+            self._nodes[nid][LATE_START] = (
+                self._critical_graph - self._nodes[nid][PATH_END] + 1
+            )
+    
+    @property
+    def critical_path(self):
+        return self._critical_graph
+
+    @property
+    def critical_path_node(self):
+        return self._critical_graph_nodes
+
+    def prioritize_nodes(self, alg):
+        return sorted([
+            (self.g[node_id], alg(*self[node_id]))
+            for node_id in self._nodes
+        ], key=lambda x: x[1])
+
+    def __getitem__(self, key):
+        return (self.g[key], self._nodes[key])
+
+    def __str__(self):
+        node_descs = []
+        edge_descs = []
+
+        for node_id in self._nodes:
+            (node, metrics) = self[node_id]
+            metrics_desc = '\\n'.join(f'{k}: {v}' for k, v in metrics.items())
+            node_desc = (
+                f'\tNode_{node.id} '
+                f'[label="{node.id} ({node.weight})\\n{metrics_desc}"];'
+            )
+            node_descs.append(node_desc)
+            for edge in node.connections_out:
+                edge_descs.append(
+                    f'\tNode_{edge.source.id} -> Node_{edge.target.id} '
+                    f'[label="{edge.weight}"];'
+                )
+        return '\n'.join([
+            'digraph GraphWithMetrics {',
+            '\n'.join(node_descs),
+            '\n'.join(edge_descs),
+            '}'
+        ])
+
+
+def alg_diff_late_early(node, metrics):
+    """Algorithm 2"""
+    return metrics[LATE_START] - metrics[EARLY_START]
+
+
+def alg_node_connectivity(node, metrics):
+    """Algorithm 10"""
+    return -metrics[CONN]
+
+
+def alg_critical_path_start(node, metrics):
+    """Algorithm 16"""
+    return metrics[PATH_START]
+
+
+if __name__ == '__main__':
+    from graph import Graph
+    g = Graph()
+    g.add_node(2)
+    g.add_node(3)
+    g.add_node(1)
+    g.add_node(3)
+    g.add_node(1)
+    g.add_node(1)
+    g.add_node(1)
+    g.add_node(4)
+    g.connect(1, 4, 1)
+    g.connect(2, 4, 1)
+    g.connect(2, 6, 1)
+    g.connect(3, 5, 1)
+    g.connect(4, 6, 1)
+    g.connect(6, 7, 1)
+    g.connect(6, 8, 1)
+    g.freeze()
+    tg = TaskGraph(g)
+    print(tg)
+    # print(tg.prioritize_nodes(alg_diff_late_early))
+    # print(tg.prioritize_nodes(alg_node_connectivity))
+    # print(tg.prioritize_nodes(alg_critical_path_start))
