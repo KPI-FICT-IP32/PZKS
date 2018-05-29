f1931d0624ad4c1e09b582c57016fadccb14fb3c
Oleksandr Kovalchuk <anxolerd@outlook.com>
2018-05-29 20:53:56 +0300

    Add more validators
    - validate_not_empty
    - validate_connected

--- semester02/Kovalchuk/validators.py
+++ semester02/Kovalchuk/validators.py
@@ -1,4 +1,15 @@
+from queue import Queue
+
+
 class CycleDetectedException(Exception):
+    pass
+
+
+class NotConnectedException(Exception):
+    pass
+
+
+class EmptyException(Exception):
     pass
 
 
@@ -29,19 +40,41 @@ def find_cycle(graph, start, visited=None):
     return None
 
 
+def validate_connected(graph):
+    visited = set()
+    todo = Queue()
+    todo.put(next(iter(graph)))
+    while not todo.empty():
+        node = todo.get()
+        visited.add(node.id)
+        for edge in node.connections_out:
+            if edge.target.id not in visited:
+                todo.put(edge.target)
+    all_nodes = set(node.id for node in graph) 
+    if all_nodes != visited:
+        not_connected_nodes = all_nodes - visited
+        raise NotConnectedException(f'Not connected: {not_connected_nodes}')
+
+
+def validate_not_empty(graph):
+    if len(graph) <= 0:
+        raise EmptyException(f'Graph is empty!')
+
+
 if __name__ == '__main__':
     from graph import Graph
-    g = Graph()
-    g.add_node(1)
-    g.add_node(2)
-    g.add_node(3)
-    g.add_node(4)
-    g.add_node(5)
-    g.add_node(6)
-    g.connect(1, 2, 1)
-    g.connect(2, 3, 1)
-    g.connect(3, 4, 1)
-    g.connect(4, 5, 1)
-    g.connect(5, 6, 1)
-    g.connect(6, 1, 1)
-    validate_acyclic(g)
+    # g = Graph()
+    # g.add_node(1)
+    # g.add_node(2)
+    # g.add_node(3)
+    # g.add_node(4)
+    # g.add_node(5)
+    # g.add_node(6)
+    # g.connect(1, 2, 1)
+    # g.connect(2, 3, 1)
+    # g.connect(3, 4, 1)
+    # g.connect(4, 5, 1)
+    # g.connect(5, 6, 1)
+    # g.connect(6, 1, 1)
+    # validate_acyclic(g)
+    # validate_connected(g)
